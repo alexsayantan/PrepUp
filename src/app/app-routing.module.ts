161a21b7403ae15aeb540e01758a4e7fab14b439
alexsayantan <sayantansaha@gmail.com>
2022-10-07 18:37:01 +0530

    Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { RegisterComponent } from './auth/register/register.component';
 import { AdmindashboardComponent } from './main/admin/admindashboard/admindashboard.component';
 import { HomeComponent } from './main/home/home.component';
 import { UserdashboardComponent } from './main/user/userdashboard/userdashboard.component';
+import { AdminguardGuard } from './services/adminguard.guard';
+import { NormalguardGuard } from './services/normalguard.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full'},
   {path: 'register', component: RegisterComponent , pathMatch: 'full'},
   {path: 'login', component: LoginComponent, pathMatch: 'full'},
   {path: 'home', component: HomeComponent, pathMatch: 'full'},
-  {path: 'admin', component: AdmindashboardComponent, pathMatch: 'full'},
-  {path: 'user', component: UserdashboardComponent, pathMatch: 'full'},
+  {path: 'admin', component: AdmindashboardComponent, pathMatch: 'full', canActivate: [AdminguardGuard]},
+  {path: 'user', component: UserdashboardComponent, pathMatch: 'full', canActivate: [NormalguardGuard]},
 
 ];
 
